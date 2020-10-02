84dbefe59ba41d4681c4a9efb37b42a79fdd1a96
swathi kunchakuri <swathi.kunchakuri@ahss.org>
2020-10-02 14:00:24 +0100

    Updates

---
+++ precache-manifest.8d7d7690d37cec582ce0599297f3661f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54bebaf81cce039ac0f8dcb7ba7b14ba",
+    "url": "/NewsPile/index.html"
+  },
+  {
+    "revision": "c63ea50ca0215df75062",
+    "url": "/NewsPile/static/css/main.8d86897b.chunk.css"
+  },
+  {
+    "revision": "80a704040366c18d77c0",
+    "url": "/NewsPile/static/js/2.102381a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NewsPile/static/js/2.102381a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c63ea50ca0215df75062",
+    "url": "/NewsPile/static/js/main.a9729f6c.chunk.js"
+  },
+  {
+    "revision": "1151000d3164a664db34",
+    "url": "/NewsPile/static/js/runtime-main.a00aaca5.js"
+  }
+]);
